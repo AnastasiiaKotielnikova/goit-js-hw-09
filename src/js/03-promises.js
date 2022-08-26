7b9cb62b4f0cff617e103fbe9d4c7928bc73f395
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-08-26 17:57:29 +0300

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,56 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const form = document.querySelector('.form');
-const delayEl = document.querySelector('[name = "delay"]');
-const stopEl = document.querySelector('[name = "stop"]');
-const amountEl = document.querySelector('[name = "amount"]');
+const btnRef = document.querySelector("[type='submit']");
+const formRef = document.querySelector('.form');
 
-form.addEventListener('submit', createPromise);
+const delayInputRef = formRef.querySelector("[name='delay']");
+const stepInputRef = formRef.querySelector("[name='step']");
+const amountInputRef = formRef.querySelector("[name='amount']");
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve({ position, delay });
-  } else {
-    reject({ position, delay });
+const labels = formRef.querySelectorAll('label');
+
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const form = evt.target;
+  const delayValue = parseInt(form.delay.value);
+  const stepValue = parseInt(form.step.value);
+  const amountValue = parseInt(form.amount.value);
+
+  for (let position = 0; position < amountValue; position += 1) {
+    const delay = delayValue + stepValue * position;
+
+    createPromise({ position, delay })
+      .then(({ position, delay } = {}) => {
+        Notify.success(`&#x2705 Fulfilled promise ${position + 1} in ${delay}ms`);
+      })
+      .catch(({ position, delay } = {}) => {
+        Notify.failure(`&#10060 Rejected promise ${position + 1} in ${delay}ms`);
+      });
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+
+  const executor = (resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  };
+
+  const promise = new Promise(executor);
+  return promise;
+}
+
+
+
+
+
