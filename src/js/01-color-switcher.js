75ab31a1ae2f7b7e791cb5ec960a0a0a1aa4e673
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-08-26 10:20:55 +0300

    add switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null; 
+
+buttonStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        body.fillStyle = getRandomHexColor;
+    }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+});
 
